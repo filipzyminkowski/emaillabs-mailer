2a38c17258a44dd024ce76be128fd9342eea11c6
mateusz.kaczmarek <mateusz.kaczmarek@globegroup.pl>
2019-12-27 14:47:46 +0100

    Support for attachments, multiple reply-to and extract code to methods.

--- src/Transport/EmailLabsTransportFactory.php
+++ src/Transport/EmailLabsTransportFactory.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace GlobeGroup\EmaillabsMailer\Transport;
+namespace GlobeGroup\EmailLabsMailer\Transport;
 
+use Symfony\Component\Mailer\Exception\IncompleteDsnException;
 use Symfony\Component\Mailer\Exception\UnsupportedSchemeException;
 use Symfony\Component\Mailer\Transport\AbstractTransportFactory;
 use Symfony\Component\Mailer\Transport\Dsn;
@@ -10,21 +11,26 @@ use Symfony\Component\Mailer\Transport\TransportInterface;
 /**
  * @author Mateusz Kaczmarek <mateusz.kaczmarek@globegroup.pl>
  */
-final class EmaillabsTransportFactory extends AbstractTransportFactory
+final class EmailLabsTransportFactory extends AbstractTransportFactory
 {
     public function create(Dsn $dsn): TransportInterface
     {
         $scheme = $dsn->getScheme();
         $appKey = $dsn->getUser();
         $secretKey = $dsn->getPassword();
-        $stmpAccount = $dsn->getOption('smtpAccount');
+        $smtpAccount = $dsn->getOption('smtpAccount');
+
+        if (!$smtpAccount) {
+            throw new IncompleteDsnException('Option "smtpAccount" is required for emaillabs transport.');
+        }
 
         if ('emaillabs+api' === $scheme) {
-            return new EmaillabsApiTransport($appKey, $secretKey, $stmpAccount, $this->client, $this->dispatcher, $this->logger);
+            return new EmailLabsApiTransport($appKey, $secretKey, $smtpAccount, $this->client, $this->dispatcher, $this->logger);
         }
 
         throw new UnsupportedSchemeException($dsn, 'emaillabs', $this->getSupportedSchemes());
     }
+
     public function supports(Dsn $dsn): bool
     {
         return 'emaillabs+api' === $dsn->getScheme();
@@ -32,6 +38,8 @@ final class EmaillabsTransportFactory extends AbstractTransportFactory
 
     protected function getSupportedSchemes(): array
     {
-        return ['emaillabs+api'];
+        return [
+            'emaillabs+api',
+        ];
     }
 }
